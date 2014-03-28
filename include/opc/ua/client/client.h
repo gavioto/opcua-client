a27d9518d65ab15d6942eacad6744352b39a51be
Olivier R-D <olivier@dummy.com>
2014-03-28 19:08:05 +0100

    add draft implementatio of high level client class

---
+++ include/opc/ua/client/client.h
@@ -0,0 +1,82 @@
+/******************************************************************************
+ *   Copyright (C) 2014-2014 by Sintef Raufoss Manufacturing                  *
+ *   olivier.roulet@gmail.com                  *
+ *                                          *
+ *   This library is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as          *
+ *   published by the Free Software Foundation; version 3 of the License.     *
+ *                                          *
+ *   This library is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.              *
+ *                                          *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the                  *
+ *   Free Software Foundation, Inc.,                          *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ ******************************************************************************/
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+
+
+#include <opc/ua/client/addon.h>
+#include <opc/ua/channel.h>
+#include <opc/ua/computer.h>
+#include <opc/common/application.h>
+#include <opc/common/node.h>
+#include <opc/common/addons_core/addon_manager.h>
+
+#include <opc/ua/protocol/binary/stream.h>
+#include <opc/ua/protocol/session.h>
+
+namespace OpcUa
+{
+  namespace Client
+  {
+
+    class Client
+    {
+        public:
+            Client(){} 
+            Client(const std::string configDir){this->configPath = configDir;} 
+            //~Client(){disconnect();}
+
+            void connect( const std::string &endpoint ); 
+            //bool connect( const char[] ); 
+            void createSession(); 
+            void disconnect(); 
+
+            void  SetSessionName(std::string str) { sessionName = str; }
+            std::string  GetSessionName() { return sessionName; }
+            std::string  GetURI() { return m_uri; }
+            void  SetURI(std::string uri) { m_uri = uri; }
+
+            std::string  GetEndpoint() { return endpoint; }
+            std::string  secPolicy() { return "None"; }
+            // Used messages mode for the secure channel, allowed: MS_None, MS_Sign, MS_SignAndEncrypt
+            //int secMessMode() { return MS_None; }
+
+            Node GetRootNode(); 
+            Node GetObjectsNode();
+            Node GetNode(NodeID nodeid); 
+
+        private:
+            OpcUa::Remote::Computer::SharedPtr server;
+            OpcUa::Application::UniquePtr application;
+            std::string  endpoint;
+            std::string  sessionName = "Open source OPC-UA Client Session";
+            std::string  m_uri = "https://github.com/treww/opc_layer.git";
+            std::string  configPath=".";
+        };
+
+
+
+
+
+  } //name space client
+} // namespace OpcUa
+
+#endif
---
+++ src/client.cpp
@@ -0,0 +1,116 @@
+/******************************************************************************
+ *   Copyright (C) 2014-2014 by Sintef Raufoss Manufacturing                  *
+ *   olivier.roulet@gmail.com                  *
+ *                                          *
+ *   This library is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU Lesser General Public License as          *
+ *   published by the Free Software Foundation; version 3 of the License.     *
+ *                                          *
+ *   This library is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.              *
+ *                                          *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the                  *
+ *   Free Software Foundation, Inc.,                          *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ ******************************************************************************/
+
+
+#include <opc/ua/client/addon.h>
+#include <opc/ua/client/client.h>
+#include <opc/common/addons_core/addon_manager.h>
+#include <opc/common/addons_core/config_file.h>
+#include <opc/common/application.h>
+#include <opc/common/node.h>
+#include <opc/common/uri_facade.h>
+#include <opc/ua/computer.h>
+#include <opc/ua/node_classes.h>
+
+#include <stdexcept>
+#include <iostream>
+
+
+
+
+
+
+
+namespace OpcUa
+{
+    namespace Client
+    {
+        void Client::connect(const std::string &endpoint)
+        {
+            //Load condfiguration
+            //const std::string configDir = configPath;
+            //const Common::ModulesConfiguration& configuration = Common::ParseConfigurationFiles(configDir);
+            Common::ModulesConfiguration configuration;
+            //Depending on endpoint we should load correct modules. Currently we only support opc.tcp anyway
+            std::cout << "Loading modules" << std::endl;
+            Common::ModuleConfiguration moduleConfig;
+            moduleConfig.ID = "opc.tcp";
+            moduleConfig.Path = "libopc_tcp_client.so";
+            configuration.push_back(moduleConfig);
+            //Now start application
+            std::cout << "Starting application" << std::endl;
+            application = OpcUa::CreateApplication();
+            std::vector<Common::AddonInformation> infos(configuration.size()); 
+            std::transform(configuration.begin(), configuration.end(), infos.begin(), std::bind(&Common::GetAddonInfomation, std::placeholders::_1));
+            application->Start(infos);
+
+
+            const Common::Uri uri(endpoint);
+            std::cout << "getting addon client: " << uri.Scheme() << std::endl;
+            const OpcUa::Client::Addon::SharedPtr addon = application->GetAddonsManager().GetAddon<OpcUa::Client::Addon>(uri.Scheme());
+            //std::shared_ptr<OpcUa::Client::Addon> addon = application->GetAddonsManager().GetAddon<OpcUa::Client::Addon>(uri.Scheme());
+            std::cout << "Connecting" << std::endl;
+            this->server = addon->Connect(endpoint);
+
+            OpcUa::Remote::SessionParameters session;
+            session.ClientDescription.URI = m_uri;
+            session.ClientDescription.ProductURI = m_uri;
+            session.ClientDescription.Name.Text = sessionName;
+            session.ClientDescription.Type = OpcUa::ApplicationType::CLIENT;
+            session.SessionName = sessionName;
+            session.EndpointURL = endpoint;
+            session.Timeout = 1200000;
+
+            std::cout << "Creating session" << std::endl;
+            server->CreateSession(session);
+            server->ActivateSession();
+            std::cout << "We are connected" << std::endl;
+            std::cout << "Endpoitns are: " << server->Endpoints() <<std::endl;
+            Node root = GetRootNode();
+            std::cout << "root is: " << root << std::endl;
+            //sleep(1);
+
+      }
+
+        void Client::disconnect()
+        {
+            std::cout << "closing session" << std::endl;
+            server->CloseSession(); 
+            application->Stop(); 
+            std::cout << "finished" << std::endl;
+        }
+
+        Node Client::GetNode(NodeID nodeid)
+        {
+            return Node(server, nodeid);
+        }
+
+        Node Client::GetRootNode()
+        {
+            return Node(server, OpcUa::ObjectID::RootFolder);
+        }
+
+        Node Client::GetObjectsNode()
+        {
+            std::cout << "Getting node object" << std::endl;
+            std::cout << server->Endpoints() << std::endl;
+            return Node(server, OpcUa::ObjectID::ObjectsFolder);
+        }
+  }
+}
